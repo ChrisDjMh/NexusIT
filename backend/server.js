4250bbadaa89d4b3cd02fe56509b70ac37fd95a1
ChrisDjMh <chrisdjmh@gmail.com>
2026-06-16 03:11:52 -0700

    Mi primer commit: Sidebar con navegación y lógica

--- backend/server.js
+++ backend/server.js
@@ -135,6 +135,13 @@ app.get('/api/usuarios', (req, res) => {
   });
 });
 
+
+app.use((req, res, next) => {
+    console.log(`[TRAFICO] Petición recibida: ${req.method} ${req.url}`);
+    next();
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
